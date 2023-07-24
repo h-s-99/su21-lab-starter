f4f19a31a22c4be8ac55f510779ab709c67f54f8
h-s-99 <huda.sajjad@10xengineers.ai>
2023-07-24 17:04:10 +0500

    Completed ex3

--- lab01/exercise3/linked_list.c
+++ lab01/exercise3/linked_list.c
@@ -1,3 +1,4 @@
+// Copyright 2023 Huda Sajjad
 #include <stdio.h>
 #include <stdlib.h>
 #include "linked_list.h"
@@ -55,9 +56,10 @@ void print_list(struct Node *head) {
 
 /* Iteratively reverses a linked list whose first node is HEAD */
 void reverse_list(struct Node **head) {
-    if (head == NULL) {
+    if (head == NULL || *head == NULL) {
         return;
     }
+
     struct Node *curr = *head;
     struct Node *next = (*head)->next;
     curr->next = NULL;
@@ -73,13 +75,15 @@ void reverse_list(struct Node **head) {
 /* Creates a new node with a data field set to DATA and adds the node
    to the back of the list pointed to by HEAD */
 void add_to_back(Node **head, int data) {
-    if (head == NULL) {
-        return;
+    if (*head == NULL) {
+        Node *new_node = create_node(data);
+        *head = new_node;
+    } else {
+        Node *new_node = create_node(data);
+        Node *prev;
+        for (Node *curr = *head; curr != NULL; curr = curr->next) {
+            prev = curr;
     }
-    Node *new_node = create_node(data);
-    Node *prev;
-    for (Node *curr = *head; curr != NULL; curr = curr->next) {
-        prev = curr;
+        prev->next = new_node;
     }
-    prev->next = new_node;
 }
