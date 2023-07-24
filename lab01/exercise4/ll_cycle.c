4aaa95ae6a0c1d7b2874893f89655772b762277c
h-s-99 <huda.sajjad@10xengineers.ai>
2023-07-24 17:45:14 +0500

    Completed ex4

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -1,6 +1,19 @@
+// Copyright 2023 Huda Sajjad
 #include <stddef.h>
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    if (head == NULL) return 0;
+    struct node *slowptr = head;
+    struct node *fastptr = head;
+    if (head->next->next == NULL) return 0;
+    while (fastptr->next != NULL && fastptr->next->next != NULL) {
+            fastptr = fastptr->next->next;
+            slowptr = slowptr->next;
+            if (fastptr == slowptr) {
+                return 1;
+            }
+    }
+    return 0;
 }
